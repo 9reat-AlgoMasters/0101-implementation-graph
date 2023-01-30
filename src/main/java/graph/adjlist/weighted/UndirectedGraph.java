b0d14acfa11e000770c35cad5156ab14d94e6c85
jhp336 <67370317+jhp336@users.noreply.github.com>
2023-01-30 20:34:28 +0900

    impl : 가중치 무방향그래프 인접리스트로 구현

--- src/main/java/graph/adjlist/weighted/UndirectedGraph.java
+++ src/main/java/graph/adjlist/weighted/UndirectedGraph.java
@@ -6,12 +6,18 @@ public class UndirectedGraph {
     
     public UndirectedGraph(int size) {
     	//TODO : 생성자 구현 -> adjList 초기화
-    	
+    	adjList = new ArrayList[size+1];
+    	for(int i = 1 ; i < size + 1; i++) {
+    		adjList[i] = new ArrayList<>();
+    	}
     }
     
     public void addEdge(int v1, int v2, int weight) {
     	//TODO : 간선 추가 메서드 구현
-    	
+    	Node node1 = new Node(v1, weight);
+    	Node node2 = new Node(v2, weight);
+    	adjList[v1].add(node2);
+    	adjList[v2].add(node1);
     }
     
     // 노드 번호와 weight를 저장하는 클래스 생성
@@ -19,9 +25,10 @@ public class UndirectedGraph {
     	int nodeNumber;
         int weight;
         
-    	public Node() {
+        public Node(int nodeNumber, int weight) {
     		// TODO: 생성자 만들기 -> 파라미터 추가 필요!
-    		
+    		this.nodeNumber = nodeNumber;
+    		this.weight = weight;
     	}
     }
 }
